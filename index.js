a771ba244e233eccc7476c1d56c91b9b088fe00a
Rezaul <mdrezaulkarrim@gmail.com>
2022-10-26 15:28:50 +0600

    added courses.json

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const courses = require("./data/courses.json");
+
 app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello from the node server 👋");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 
 app.listen(port, () => {
